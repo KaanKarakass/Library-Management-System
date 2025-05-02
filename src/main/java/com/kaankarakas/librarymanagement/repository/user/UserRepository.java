1a576e3ff76b80daf043157ac6ee31f6a7e14dd9
Kaan Karakaş <kaan.karakass1@outlook.com>
2025-05-02 03:00:13 +0300

    Update CustomUserDetailsService

--- src/main/java/com/kaankarakas/librarymanagement/repository/user/UserRepository.java
+++ src/main/java/com/kaankarakas/librarymanagement/repository/user/UserRepository.java
@@ -4,10 +4,14 @@ import com.kaankarakas.librarymanagement.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByEmail(String email);
 
     boolean existsByUsername(String userName);
+
+    Optional<User> findByUsername(String username);
 }
--- src/main/java/com/kaankarakas/librarymanagement/security/CustomUserDetailsService.java
+++ src/main/java/com/kaankarakas/librarymanagement/security/CustomUserDetailsService.java
@@ -1,23 +1,39 @@
 package com.kaankarakas.librarymanagement.security;
 
-import org.springframework.security.core.userdetails.User;
+import com.kaankarakas.librarymanagement.api.exception.LibraryException;
+import com.kaankarakas.librarymanagement.domain.user.User;
+import com.kaankarakas.librarymanagement.repository.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import static com.kaankarakas.librarymanagement.validator.user.UserServiceValidationRule.ERR_USER_NOT_FOUND;
+
 @Component
+@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        if (!username.equals("user")) {
-            throw new UsernameNotFoundException("User not found");
-        }
-        return User.builder()
-                .username("user")
-                .password(new BCryptPasswordEncoder().encode("password"))
-                .roles("ADMIN")
+        User user = checkUserByUsername(username);
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRole().name())
                 .build();
+
+    }
+
+    private User checkUserByUsername(String username) {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new LibraryException(ERR_USER_NOT_FOUND.getDescription(), HttpStatus.NOT_FOUND));
     }
 }
